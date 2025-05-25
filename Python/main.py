68434a897697890ee81aab02d56f71a78cb939a2
AlirezaRajabali <alirezarajabali041@gmail.com>
2025-05-25 05:08:58 -0700

    main.py

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,9 @@
 from configparser import ConfigParser
 from javascript import require, On
-mineflayer = require('mineflayer')
-from tkinter import ttk
-from tkinter import Tk, Label
 import threading, os
 from sys import platform
+
+mineflayer = require('mineflayer')
 
 ### By Fortcote
 ### https://discord.gg/bjgpVAxgyE
@@ -14,33 +13,35 @@ config.read('config.ini')
 
 def started(stop):
     bot = mineflayer.createBot({
-      'host': config.get('server', 'host'),
-      'port': config.get('server', 'port'),
-      'username': config.get('bot', 'name')    })
+        'host': config.get('server', 'host'),
+        'port': config.get('server', 'port'),
+        'username': config.get('bot', 'name')
+    })
     print('Start')
-    
+
     @On(bot, "login")
     def login(this):
         bot.chat(config.get('bot', 'register'))
         bot.chat(config.get('bot', 'login'))
-        bm.configure(text='Bot online', font=('Arial Black', 20))
-        
+        print("Bot online")
+
     @On(bot, "error")
     def error(err, *a):
         print("Connect ERROR: ", err, a)
-    
+
     @On(bot, "kicked")
     def kicked(this, reason, *a):
         print("I was kicked: ", reason, a)
-        print('reconnect')
+        print('Reconnecting...')
         bot.end(); bot.join()
-    
+
     @On(bot, "chat")
     def handle(this, username, message, *args):
         if username == bot.username:
             return
         elif message.startswith(config.get('command', 'position')):
-            p = bot.entity.position; bot.chat(f"Bot > I am at {p.toString()}")
+            p = bot.entity.position
+            bot.chat(f"Bot > I am at {p.toString()}")
         elif message.startswith(config.get('command', 'start')):
             bot.chat('24 ATERNOS > Bot started! - Made By Fortcote')
             bot.setControlState('forward', True)
@@ -49,11 +50,11 @@ def started(stop):
         elif message.startswith(config.get('command', 'stop')):
             bot.chat('24 ATERNOS > Bot stoped! - Made By Fortcote')
             bot.clearControlStates()
-    
+
     @On(bot, "spawn")
     def spawn(this):
         bot.chat("Bot > Spawned!")
-    
+
     @On(bot, "death")
     def death(this):
         bot.chat("Bot > Respawn!")
@@ -61,7 +62,7 @@ def started(stop):
 def start():
     global bott, stop_threads
     stop_threads = False
-    bott = threading.Thread(target=started, args=(lambda: stop_threads, ))
+    bott = threading.Thread(target=started, args=(lambda: stop_threads,))
     bott.start()
 
 def stop():
@@ -70,23 +71,9 @@ def stop():
             os.system('taskkill /f /im node.exe')
         else:
             os.system('killall node')
-        bm.configure(text='Bot offline', font=('Arial Black', 20))
+        print("Bot offline")
     except:
         pass
 
-root = Tk()
-
-root.tk.call('source', 'forest-dark.tcl')
-ttk.Style().theme_use('forest-dark')
-
-root.geometry('250x150')
-root.resizable(width=False, height=False)
-root.title('24 ATERNOS - V.2')
-
-ttk.Button(root, text='Start', command=lambda:start()).place(x=5, y=100)
-ttk.Button(root, text='Stop', command=lambda:stop()).place(x=151, y=100)
-bm = Label(root, text='Bot offline', font=('Arial Black', 20))
-bm.place(x=50, y=30)
-
-
-root.mainloop()
+if __name__ == '__main__':
+    start()
